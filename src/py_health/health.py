7b107b27505c6316abd4165ae0d414ef6c154b6e
Preston Van Loon <preston@machinepowered.com>
2018-07-14 09:07:08 -0400

    Add a py_binary for testing/demo

--- src/py_health/health.py
+++ src/py_health/health.py
@@ -14,7 +14,13 @@
 """Reference implementation for health checking in gRPC Python."""
 
 import threading
+import grpc
 
-from src.py_health import health_pb2
-from src.py_health import health_pb2_grpc
+from protos.health import health_pb2
+from protos.health import health_pb2_grpc
 
+def run():
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = health_pb2_grpc.HealthStub(channel)
+    response = stub.Check(health_pb2.HealthCheckRequest())
+    print("Health client received: " + response.message)
